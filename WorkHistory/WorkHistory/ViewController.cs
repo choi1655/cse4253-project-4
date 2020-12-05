acea05d6dfff38a18c6733de13570fa454d5c363
choi1655 <choi.1655@osu.edu>
2020-12-05 06:58:35 -0500

    Main screen structure complete

--- WorkHistory/WorkHistory/ViewController.cs
+++ WorkHistory/WorkHistory/ViewController.cs
@@ -1,6 +1,7 @@
 ﻿using Foundation;
 using System;
 using UIKit;
+using SafariServices;
 
 namespace WorkHistory
 {
@@ -16,13 +17,19 @@ namespace WorkHistory
             // Perform any additional setup after loading the view, typically from a nib.
             //titleLabel.Hidden = false;
             titleLabel.TextColor = UIColor.Black;
-            Console.WriteLine("Running...");
         }
 
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void websitePressed(UIButton button)
+        {
+            var url = new NSUrl("https://johnchoi96.github.io/");
+            var sfViewController = new SFSafariViewController(url);
+            PresentViewController(sfViewController, true, null);
         }
     }
 }
