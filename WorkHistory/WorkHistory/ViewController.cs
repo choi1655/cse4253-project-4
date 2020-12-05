506b00b9203f0ee4682f649d6ee58b668886d0e4
= <=>
2020-12-05 06:13:16 -0500

    Initial commit with just a label

---
+++ WorkHistory/WorkHistory/ViewController.cs
@@ -0,0 +1,28 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace WorkHistory
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            //titleLabel.Hidden = false;
+            titleLabel.TextColor = UIColor.Black;
+            Console.WriteLine("Running...");
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
